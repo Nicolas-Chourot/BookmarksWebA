9b656c03d2d4cbf57b5287abf45dd32a6e8f66e7
Chourot Nicolas <nicolaschourot@videotron.ca>
2021-01-28 14:32:44 -0500

    Many changes

---
+++ Data/BookmarksWebAContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookmarksWebA.Models;
+
+namespace BookmarksWebA.Data
+{
+    public class BookmarksWebAContext : DbContext
+    {
+        public BookmarksWebAContext (DbContextOptions<BookmarksWebAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookmarksWebA.Models.Bookmark> Bookmark { get; set; }
+    }
+}
