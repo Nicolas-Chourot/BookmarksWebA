2b5379aeffdb1bdd36131510cfa62746616dc958
Chourot Nicolas <nicolaschourot@videotron.ca>
2021-01-28 13:59:21 -0500

    Add Annotation on Bookmark members

--- Models/Bookmark.cs
+++ Models/Bookmark.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace BookmarksWebA.Models
     public class Bookmark
     {   // prop
         // ctor
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
+        [Url]
         public string Url { get; set; }
 
         public Bookmark()
